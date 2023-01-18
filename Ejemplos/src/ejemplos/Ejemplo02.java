9bfd5ebbc156f7d7f44c187d9f1e28a1558c5b1f
Soffi <psvire@utpl.edu.ec>
2023-01-18 14:52:32 -0500

    ramdom

--- Ejemplos/src/ejemplos/Ejemplo02.java
+++ Ejemplos/src/ejemplos/Ejemplo02.java
@@ -6,6 +6,7 @@
 package ejemplos;
 
 import java.security.SecureRandom;
+import java.util.Scanner;
 // import java.security.*;
 
 /**
@@ -13,30 +14,35 @@ import java.security.SecureRandom;
  * @author reroes
  */
 public class Ejemplo02 {
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
-        int valorA = obtenerNumero(); // 3
-        int valorB = obtenerNumero(); // 2
+        Scanner entrada = new Scanner(System.in);
+        int limite = 0;
+
+        System.out.println("Ingrese el limite de numeros a generar");
+        limite = entrada.nextInt();
+        int valorA = obtenerNumero(limite); // 3
+        int valorB = obtenerNumero(limite); // 2
         int suma = obtnerSuma(valorA, valorB);
-        System.out.printf("La suma de %d + %d es igual a: %d\n", 
+        System.out.printf("La suma de %d + %d es igual a: %d\n",
                 valorA,
                 valorB,
                 suma);
     }
-    
-    public static int obtenerNumero() {
+
+    public static int obtenerNumero(int l) {
         // objeto generador de números aleatorios
         SecureRandom numerosAleatorios = new SecureRandom();
-        
+
         // Returns a pseudorandom, uniformly distributed int value 
         // between 0 (inclusive) and the specified value (exclusive)
-        int valorAleatorio = numerosAleatorios.nextInt(5);
+        int valorAleatorio = numerosAleatorios.nextInt(l);
         return valorAleatorio;
     }
-    
-    public static int obtnerSuma(int a, int b){
+
+    public static int obtnerSuma(int a, int b) {
         return a + b;
     }
-    
+
 }
